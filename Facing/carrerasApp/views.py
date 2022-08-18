e0abe3777a4c2cdee16e2905cf5e9f4252414248
Gerald Ayala <geraldayala87@gmail.com>
2022-08-18 14:01:57 -0500

    Carreras app

--- Facing/carrerasApp/views.py
+++ Facing/carrerasApp/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render
 from carrerasApp.models import *
 # Create your views here.
 def listarcategoria(request,id):
-    queryset=request.GET.get("buscar")
-    categoria=Categoria.objects.filter(estado=True) 
-    if queryset:
-        categoria=Categoria.objects.filter(Q(descripcion__icontains=queryset),estado=True).distinct() 
-    context={'categoria':categoria}
+    escuela= Escuelas.objects.get(idescuela = id)
+    docentes = Docentes.objects.get(idEscuela = id)
+    grados = Docentes.objects.filter()
     return render(request,"categoria/listar.html",context) 
