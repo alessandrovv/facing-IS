2d09c049520f9cd5801e409e72df5871daf0b0b1
INameless <aleleonhart01@gmail.com>
2022-08-22 08:24:46 -0500

    addcom

--- Facing/Facing/settings.py
+++ Facing/Facing/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'Facing.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'facing',
+        'NAME': 'dbfacing',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
